724f548f20ac420d217d38bd5c8e18fb22483c56
meowlet <ngoclink13@gmail.com>
2024-03-31 21:06:34 +0700

    Project initializing

--- app/src/main/java/com/meow/google/MainActivity.kt
+++ app/src/main/java/com/meow/google/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.meow.google.ui.theme.GoogleTheme
+import com.meow.google.presentation.ui.theme.GoogleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
